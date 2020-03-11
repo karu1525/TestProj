7e63c4dcc333482987caf5f9c86a67278e911f87
pmoko <pmoko@LAPTOP-HC0HJ95I>
2020-03-12 00:58:29 +0530

    initial commit with projv1.0

---
+++ Practo_demo/src/main/java/com/example/demo/SwapeVar.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+public class SwapeVar {
+
+	public static void main(String[] args) {
+	
+		int x = 10;
+		int y= 7;
+		
+		x = x +y;
+		y=x-y;
+		
+		x=x-y;
+		
+		System.out.println("x: " +x );
+		System.out.println("y: "+y );
+		
+		
+				
+
+	}
+
+}
+
